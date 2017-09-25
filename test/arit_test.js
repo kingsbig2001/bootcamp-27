42c87c3ad37d470a54ed4bb7af38071f835d8224
Ibeh Kingsley <kingsbig2001@yahoo.ca>
2017-09-25 20:17:35 +0100

    refactored functions in test cases to es6 arrow notation

--- test/arit_test.js
+++ test/arit_test.js
@@ -1,38 +1,38 @@
 const assert = require("assert");
 const aritGeo = require("../aritgeo");
 
-describe('test for aritGeo function', function(){
+describe('test for aritGeo function', () => {
  
- it("no arguement should return -1", function(){
+ it("no arguement should return -1", () => {
     assert(aritGeo() === -1);
  });
  
- it("parsing a string intead of array should return -1", function(){
+ it("parsing a string intead of array should return -1", () => {
     assert(aritGeo('array') === -1);
  });
-    it("empty array should return 0", function(){
+    it("empty array should return 0", () => {
     assert(aritGeo([]) === 0);
  });
-    it("array with string should return -1", function(){
+    it("array with string should return -1", () => {
     assert(aritGeo(['str', 1, 3, 5]) === -1);
  });
-     it("should return Geometric", function(){
+     it("should return Geometric", () => {
     assert(aritGeo([2, 4, 8, 16]) === 'Geometric');
  });
-     it("should return Arithmethic", function(){
+     it("should return Arithmethic", () =>{
     assert(aritGeo([1, 3, 5, 7]) === 'Arithmethic');
  });
-     it("array not a Geometric or Arithmetic Sequence should return -1", function(){
+     it("array not a Geometric or Arithmetic Sequence should return -1", () => {
     assert(aritGeo([4, 7, 9, 22, 44]) === -1);
  });
 
-    it("parsing list of numbers instead of array should return -1", function(){
+    it("parsing list of numbers instead of array should return -1", () => {
     assert(aritGeo(44, 47, 50, 53) === -1);
  });
-    it("array of strings should return -1", function(){
+    it("array of strings should return -1", () => {
     assert(aritGeo(['er', 'str', 'ob', 'kk']) === -1);
  });
-    it("parsing an object should return  -1", function(){
+    it("parsing an object should return  -1", () =>{
     assert(aritGeo({}) === -1);
  });
 });
