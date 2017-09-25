7c6547be76f4e70020af5ce6622ce29275c4377c
Ibeh Kingsley <kingsbig2001@yahoo.ca>
2017-09-25 22:52:24 +0100

    used module.exports to export aritgeo function

--- aritgeo.js
+++ aritgeo.js
@@ -1,37 +1,38 @@
- const aritGeo = (arr) =>{
-  if(!arr){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var aritGeo = function aritGeo(arr) {
+  if (!arr) {
     return -1;
   }
   //check if arguement parsed is an array
-  if(!Array.isArray(arr)){
+  if (!Array.isArray(arr)) {
     return -1;
   }
   //check for empty array
-  if(arr.length < 1){
-  return 0;
+  if (arr.length < 1) {
+    return 0;
   }
   //make sure array has
-  else if(arr.length > 2 ){
-    const diff = arr[2] -arr[1];
-    
-    for(let i = 0; i < arr.length; i++){
-      //make sure only numbers are in array
-      console.log(arr[i]);
-      if(typeof arr[i] !== 'number'){
-      return -1;
+  else if (arr.length > 2) {
+      var diff = arr[2] - arr[1];
+
+      for (var i = 0; i < arr.length; i++) {
+        //make sure only numbers are in array
+        console.log(arr[i]);
+        if (typeof arr[i] !== 'number') {
+          return -1;
+        } else if (Math.pow(arr[1], 2) === arr[0] * arr[2]) {
+          return 'Geometric';
+        } else if (arr[i] === arr[1] + (i - 1) * diff) {
+          return 'Arithmethic';
+        } // {a[n]=a[1]+(n-1)d},
+        else {
+            return -1;
+          }
       }
-  
-      else if(Math.pow(arr[1], 2) === arr[0]*arr[2]){
-       return 'Geometric';
-       }
-      else if(arr[i] === (arr[1] + ((i -1) * diff))){
-       return 'Arithmethic';
-       } // {a[n]=a[1]+(n-1)d},
-    else{
-     return -1;
-     }
     }
-  
-  }
-}
- export default aritGeo;
+};
+exports.default = aritGeo;
