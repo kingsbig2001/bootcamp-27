65fee280b2d841ba581af58c44967fd14b8bed1b
Ibeh Kingsley <kingsbig2001@yahoo.ca>
2017-09-26 14:40:47 +0100

    added a method to manipulate account bal

--- oop/oop.js
+++ oop/oop.js
@@ -4,6 +4,7 @@ class Person {
   constructor(name, status) {
     this._name = name;
     this._status = status;
+    
   }
   //setting the getter Method
   get name() {
@@ -11,7 +12,9 @@ class Person {
   }
   //setting the setter Method
   set name(newName) {
+    if(newName.length < 20){
     this._name = newName;
+    }
   }
   get status(){
     return this._status;
@@ -21,15 +24,17 @@ class Person {
     this._status = newStatus;
   }
   
+  
   hired() {
     console.log(this._name + ` is currently ${this._status}`);
   }
 }
 //child class 'Engineer' Inheriting from Parent class 'Person'
 class Engineer extends Person { 
-  constructor(name,status, field) {
+  constructor(name,status, field, accountBal) {
     super(name, status);
     this._field = field;
+    this._accountBal = accountBal;
   }
   
   get field() {
@@ -39,17 +44,36 @@ class Engineer extends Person {
   set field(newField) {
     this._field = newField;
   }
+  get accountBal(){
+    return this._accountBal;
+  }
   
+  set accountBal(newBal){
+    let previousBal = this._accountBal;
+    if(newBal && typeof newBal === 'number'){
+      this._accountBal = newBal; 
+    }
+    else{
+      this._accountBal = previousBal;
+    }
+  }
   working() {
     console.log(this._name + ' working in ' + this._field + ' dept.');
   }
 }
 
 let ibeh = new Person('ibeh', 'hired');
+console.log(ibeh);
+
 ibeh.hired();
 ibeh.status = 'unemployed';
 console.log(ibeh.hired());
-let ebube = new Engineer('Ebube', 'hired', 'Electrical');
+let ebube = new Engineer('Ebube', 'hired', 'Electrical', 99);
 ebube.hired();
 ebube.working();
-console.log(ebube.name);
+console.log(ebube);
+ebube.accountBal;
+ebube.accountBal = 9000;
+ebube.accountBal;
+ebube.accountBal = 'omo';
+ebube.accountBal;
